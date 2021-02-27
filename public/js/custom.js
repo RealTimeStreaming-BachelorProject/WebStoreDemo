5d30eeb482da5b5d2db6c8e123e878e5b78a7e30
omvk97 <olvan18@student.sdu.dk>
2021-02-27 21:40:04 +0100

    Checkout request to packageservice

---
+++ public/js/custom.js
@@ -0,0 +1,20 @@
+const packageServiceEndpoint = "http://localhost:9999/packages";
+async function onCheckout() {
+  const body = {
+    receiverAddress: document.getElementById("street_address").value,
+    receiverName:
+      document.getElementById("first_name").value +
+      document.getElementById("last_name").value,
+    receiverEmail: document.getElementById("email_address").value,
+    senderAddress: "Høgevej 25, 8210 Aarhus",
+    senderName: "KARL FASHION",
+    weightKg: 2,
+  };
+  const response = await fetch(packageServiceEndpoint, {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  }).then((res) => res.json());
+
+  console.log(response);
+}
